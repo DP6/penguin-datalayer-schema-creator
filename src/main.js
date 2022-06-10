ef8cd621632e254f8edabad594db5fc626f771c7
Gabriel Tellaroli Ramos <bi.tellaroli1@hotmail.com>
2022-06-10 13:18:20 -0300

    feat: :sparkles: Funcionalidade de adicionar e remover eventos implementada.
    Funcionalidade de adicionar e remover eventos implementada, e visualização do schema em tempo real.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,69 @@
-let schemaItens = [];
 let btnAddEvent = document.querySelector('#buttonAddEvent');
+let btnRemoveEvent = document.querySelector('#buttonRemoveEvent');
+let eventList = document.querySelector('#eventSelected');
+let schemaParagraphy = document.querySelector('#schemaExample');
+
+let schema = {
+  "$schema": "",
+  "title": "The Root Schema",
+  "array": {
+    "$id": "#/properties/schema",
+    "type": "array",
+    "items": []
+  }
+};
+
+let updateSchemaExample = (schema) => {
+  schemaParagraphy.textContent = schema;
+}
+
+let removeEventInSchema = (eventName, eventIndex) => {
+
+  schema.array.items.splice(eventIndex, 1);
+
+};
 
 btnAddEvent.onclick = () => {
-    let eventName = document.querySelector('#eventName').value;
+  let eventName = document.querySelector('#eventName').value;
+  let newEvent = new Option(eventName, eventName);
 
-    if (eventName == '') {
-        alert('Please enter a valid Name');
-        return;
-    }
+  document.querySelector('#eventName').value = '';
 
-    let eventObjt = {
-        "type": "object",
-        "properties": {
-          "event": {
-            "type": "string",
-            "enum": [eventName]
-          }
-        },
-        "required": ["event"]
+  if (eventName == '') {
+    alert('Please enter a valid Name');
+    return;
+  }
+
+  let eventObjt = {
+    "type": "object",
+    "properties": {
+      "event": {
+        "type": "string",
+        "enum": [eventName]
       }
+    },
+    "required": ["event"]
+  }
+  schema.array.items.push(eventObjt);
+  eventList.add(newEvent, undefined);
 
-    schemaItens.push(eventObjt)
+  updateSchemaExample(JSON.stringify(schema));
 
-    console.log(schemaItens);
+}
+
+eventList.addEventListener('change', () => {
+  btnRemoveEvent.hidden = false;
+});
+
+btnRemoveEvent.onclick = () => {
+
+  removeEventInSchema(eventList.value, eventList.selectedIndex - 1);
+  eventList.remove(eventList.selectedIndex);
+
+  if (eventList.length == 1) {
+    eventList[0].selected = true;
+    btnRemoveEvent.hidden = true;
+  };
+
+  updateSchemaExample(JSON.stringify(schema));
 }
