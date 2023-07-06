3889bfd0f0350b5d29a8e1f4884433f4d50db544
FernandoRF <fernandoramiresfilho@gmail.com>
2023-07-06 18:57:32 -0300

    configuração do Jekyll e ajustes no funcionamento das páginas

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,13 @@
 const schemaParagraphy = document.querySelector('#schemaExample');
+let schema = {
+  $schema: '',
+  title: 'The Root Schema',
+  array: {
+    $id: '#/properties/schema',
+    type: 'array',
+    items: [],
+  },
+};
 
 /* Event Variables */
 const btnAddEvent = document.querySelector('#buttonAddEvent');
@@ -6,10 +15,20 @@ const btnRemoveEvent = document.querySelector('#buttonRemoveEvent');
 const btnEditEvent = document.querySelector('#buttonEditEvent');
 const eventList = document.querySelector('#eventSelected');
 /* *************** */
-
+//edit event
+const addEdit = document.querySelector('#buttonAddEventPopup');
+const cancelEdit = document.querySelector('#buttonCancelEventPopup');
 /* Property Variables */
 const btnAddProperty = document.querySelector('#buttonAddProperty');
+const propertyNv2 = document.querySelector('#propertyNv2');
+const propertyNv3 = document.querySelector('#propertyNv3');
+const propertyNv4 = document.querySelector('#propertyNv4');
+const propertyNv5 = document.querySelector('#propertyNv5');
 const btnRemoveProperty = document.querySelector('#buttonRemoveProperty');
+const btnRemovePropertyNv2 = document.querySelector('#buttonRemovePropertyNv2');
+const btnRemovePropertyNv3 = document.querySelector('#buttonRemovePropertyNv3');
+const btnRemovePropertyNv4 = document.querySelector('#buttonRemovePropertyNv4');
+const btnRemovePropertyNv5 = document.querySelector('#buttonRemovePropertyNv5');
 const btnEditProperty = document.querySelector('#buttonEditProperty');
 const propertyList = document.querySelector('#propertySelected');
 
@@ -19,21 +38,77 @@ const propertyValidation = document.querySelector('#newPropertyValidation');
 const propertyRequired = document.querySelector('#newPropertyRequired');
 const propertyValue = document.querySelector('#newPropertyValue');
 
+//edit property
+
+/* Obj button */
+
+const btnAddObjectNv2 = document.querySelector('#buttonNv2');
+const btnAddObjectNv3 = document.querySelector('#buttonNv3');
+const btnAddObjectNv4 = document.querySelector('#buttonNv4');
+const btnAddObjectNv5 = document.querySelector('#buttonNv5');
+
+//Add In Obj
+
+/* Add config button */
+
+const btnAddConfig = document.querySelector('#buttonAddConfig');
+const btnRemoveConfig = document.querySelector('#buttonRemoveConfig');
+
+//Popup
+const popupProperties = document.querySelector('.popup-wrapperProperties');
+const closeProperties = document.querySelector('.popup-closeProperties');
+const popup = document.querySelector('.popup-wrapper');
+const closePopup = document.querySelector('.popup-close');
+
+//property popup buttons
+const btnAddPropertyPopup = document.querySelector('#buttonAddPropertyPopup');
+const btnAddPropertyPopupNv2 = document.querySelector('#buttonAddPropertyNv2Popup');
+const btnAddPropertyPopupNv3 = document.querySelector('#buttonAddPropertyNv3Popup');
+const btnAddPropertyPopupNv4 = document.querySelector('#buttonAddPropertyNv4Popup');
+const btnAddPropertyPopupNv5 = document.querySelector('#buttonAddPropertyNv5Popup');
+const btnCancelPropertyPopup = document.querySelector('#buttonCancelPropertyPopup');
+
+//event popup buttons
+
 /* Filtro para trazer somente o elemento que está marcado */
-const propertyPlace = Array.from(document.querySelectorAll("#newPropertyPlace")).filter((item) => { return item.checked })[0];
+//const propertyPlace = Array.from(document.querySelectorAll("#newPropertyPlace")).filter((item) => { return item.checked })[0];
 
 /* ****************** */
 
-let schema = {
-  "$schema": "",
-  "title": "The Root Schema",
-  "array": {
-    "$id": "#/properties/schema",
-    "type": "array",
-    "items": []
-  }
+let validator = {
+  validator: [
+    {
+      url: '',
+      schema_name: [''],
+      dataLayer: '',
+      browserClose: true,
+      time: 0,
+    },
+  ],
 };
-
+/**
+ * Updates the schema into the website.
+ * @param {string} schema - The new schema to be updated.
+ * @return {string} Returns the new schema.
+ */
 const updateSchemaExample = function (schema) {
   schemaParagraphy.textContent = schema;
-}
+};
+
+/**
+ * Updates the schema into the website.
+ * @param {string} validator - The new schema to be updated.
+ * @return {string} Returns the new schema.
+ */
+const updateValidatorExample = function (validator) {
+  schemaParagraphy.textContent = validator;
+};
+
+// label names
+
+const label2 = document.querySelector('#propertyLabel2');
+const label3 = document.querySelector('#propertyLabel3');
+const label4 = document.querySelector('#propertyLabel4');
+const label5 = document.querySelector('#propertyLabel5');
+
+const schemaName = document.querySelector('#schema_name');
--- public/scripts/properties.js
+++ public/scripts/properties.js
@@ -1,137 +1,116 @@
 const removePropertyInSchema = function (propertyName, propertyIndex) {
+  //schema.array.items[eventList.selectedIndex-1].properties.delete(propertyIndex,1)
 };
 
 const resetPropertyValues = function () {
-    propertyName.value = '';
-    propertyValue.value = '';
-    propertyType[0].selected = true;
-    propertyRequired[0].selected = true;
-    propertyValidation.hidden = true;
+  propertyName.value = '';
+  propertyValue.value = '';
+  propertyType[0].selected = true;
+  propertyRequired[0].selected = true;
+  propertyValidation.hidden = true;
 };
 
 btnAddProperty.onclick = function () {
+  btnAddObjectNv2.style.display = 'block';
+  let newProperty = new Option(propertyName.value, propertyName.value);
 
-    let newProperty = new Option(propertyName.value, propertyName.value);
+  if (
+    propertyName.value === '' ||
+    (propertyValue.value === '' && propertyType.value === 'string') ||
+    propertyType.value === 'Property Type' ||
+    propertyRequired.value === 'Property Mode'
+  ) {
+    alert('Please enter all values !');
+    return;
+  }
 
-    if (
-        propertyName.value === ''
-        || (propertyValue.value === '' && propertyType.value === 'string')
-        || propertyType.value === 'Property Type'
-        || propertyRequired.value === 'Property Mode'
-    ) {
-        alert('Please enter all values !');
-        return;
-    }
+  let eventIndex = eventList.selectedIndex - 1;
 
-    let eventIndex = eventList.selectedIndex - 1;
+  switch (propertyType.value) {
+    case 'string':
+      if (propertyValidation.value === 'enum') {
+        schema.array.items[eventIndex].properties[propertyName.value] = {
+          type: 'string',
+          enum: propertyValue.value.split(','),
+        };
+      } else {
+        schema.array.items[eventIndex].properties[propertyName.value] = {
+          type: 'string',
+          pattern: propertyValue.value,
+        };
+      }
+      break;
 
-    if (propertyPlace.value === 'event') {
-        switch (propertyType.value) {
-            case 'string':
+    case 'number':
+      schema.array.items[eventIndex].properties[propertyName.value] = {
+        type: 'number',
+      };
+      break;
 
-                if (propertyValidation.value === 'enum') {
-                    schema.array.items[eventIndex]
-                        .properties[propertyName.value] =
-                    {
-                        "type": "string",
-                        "enum": propertyValue.value.split(',')
-                    }
-                } else {
-                    schema.array.items[eventIndex]
-                        .properties[propertyName.value] =
-                    {
-                        "type": "string",
-                        "pattern": propertyValue.value
-                    }
-                }
-                break;
+    case 'boolean':
+      schema.array.items[eventIndex].properties[propertyName.value] = {
+        type: 'boolean',
+      };
+      break;
 
-            case 'number':
-                schema.array.items[eventIndex]
-                    .properties[propertyName.value] =
-                {
-                    "type": "number"
-                }
-                break;
+    case 'object':
+      schema.array.items[eventIndex].properties[propertyName.value] = {
+        type: 'object',
+        properties: {},
+        required: [],
+      };
 
-            case 'boolean':
-                schema.array.items[eventIndex]
-                    .properties[propertyName.value] =
-                {
-                    "type": "boolean"
-                }
-                break;
+      break;
 
-            case 'object':
-                schema.array.items[eventIndex]
-                    .properties[propertyName.value] =
-                {
-                    "type": "object",
-                    "properties": {
-                    },
-                    "required": [
-                    ]
-                }
+    case 'array':
+      schema.array.items[eventIndex].properties[propertyName.value] = {
+        type: 'array',
+        contains: {},
+      };
 
-                break;
+      break;
 
-            case 'array':
-                schema.array.items[eventIndex]
-                    .properties[propertyName.value] =
-                {
-                    "type": "array",
-                    "contains": {
-                    }
-                }
+    default:
+      break;
+  }
 
-                break;
+  if (propertyRequired.value === 'required') {
+    schema.array.items[eventIndex].required.push(propertyName.value);
+  }
 
-            default:
-                break;
-        }
-    }
-    else {
-
-    }
-
-    if (propertyRequired.value === 'required') {
-        schema.array.items[eventIndex].required.push(propertyName.value)
-    }
-
-    propertyList.add(newProperty);
-    updateSchemaExample(JSON.stringify(schema, undefined, 2));
-    resetPropertyValues();
-}
+  propertyList.add(newProperty);
+  updateSchemaExample(JSON.stringify(schema, undefined, 2));
+  resetPropertyValues();
+};
 
 propertyType.addEventListener('change', () => {
-    if (propertyType.value === "string") {
-        propertyValidation.hidden = false;
-        propertyValue.hidden = false;
-    } else {
-        propertyValidation.hidden = true;
-        propertyValue.hidden = true;
-    }
+  if (propertyType.value === 'string') {
+    propertyValidation.hidden = false;
+    propertyValue.hidden = false;
+  } else {
+    propertyValidation.hidden = true;
+    propertyValue.hidden = true;
+  }
 });
 
 propertyList.addEventListener('change', () => {
-    btnRemoveProperty.hidden = false;
-    btnEditProperty.hidden = false;
+  btnRemoveProperty.hidden = false;
+  btnEditProperty.hidden = false;
 });
 
 btnRemoveProperty.onclick = function () {
+  let selectProps = document.querySelectorAll('#propertySelected')[0];
+  let selectEvents = document.querySelectorAll('#eventSelected')[0];
+  let selectedProperty = selectProps.children[selectProps.selectedIndex].value;
+  delete schema.array.items[selectEvents.selectedIndex - 1].properties[selectedProperty];
+  propertyList.remove(propertyList.selectedIndex);
+  schema.array.items[selectEvents.selectedIndex - 1].required.splice(selectProps.selectedIndex, 1);
 
-    removeEventInSchema(propertyList.value, propertyList.selectedIndex - 1);
-    propertyList.remove(propertyList.selectedIndex);
+  if (propertyList.length === 1) {
+    propertyList[0].selected = true;
+    btnRemoveProperty.hidden = true;
+    btnEditProperty.hidden = true;
+  }
 
-    if (propertyList.length === 1) {
-        propertyList[0].selected = true;
-        btnRemoveProperty.hidden = true;
-        btnEditProperty.hidden = true;
-    };
-
-    updateSchemaExample(JSON.stringify(schema, undefined, 2));
-}
-
-btnEditProperty.onclick = function () {
-
-}
+  updateSchemaExample(JSON.stringify(schema, undefined, 2));
+};
